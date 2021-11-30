1f6b1ea46f1e534a9a81903aad92620d512999c7
Carl-Johan Lindblad <cj.lindblad@caretotranslate.com>
2021-11-30 20:30:34 +0100

    Added function for reading all text from input file.

--- aoc-2021/InputReader.Tests.fs
+++ aoc-2021/InputReader.Tests.fs
@@ -2,12 +2,19 @@ module InputReader_Tests
 
 open Xunit
 open InputReader
+open Xunit.Sdk
 
 [<Fact>]
 let ``Can read input lines from file`` () =
     let result = readLines "./sample-text-input"
     let expected = ["one line"; "two line"; "three line"]
     Assert.Equal<string list>(expected, result)
+
+[<Fact>]
+let ``Can read whole input`` () =
+    let result = readText "./sample-text-input"
+    let expected = "one line\ntwo line\nthree line"
+    Assert.Equal<string>(expected, result)
 
 [<Fact>]
 let ``Can read lines of numbers from file`` () =
--- aoc-2021/InputReader.fs
+++ aoc-2021/InputReader.fs
@@ -3,5 +3,8 @@ module InputReader
 let readLines filePath =
     System.IO.File.ReadLines(filePath) |> List.ofSeq
 
+let readText filePath =
+    System.IO.File.ReadAllText(filePath)
+
 let readNumberLines filePath =
     readLines filePath |> List.map int
