d2b4aba52d97418a0e1f4f36398cd9d5d3981565
Carl-Johan Lindblad <cj.lindblad@caretotranslate.com>
2021-12-01 09:28:10 +0100

    Day 1 part 1 done.

--- aoc-2021/01/Day1.Tests.fs
+++ aoc-2021/01/Day1.Tests.fs
@@ -4,9 +4,15 @@ open Xunit
 open Day1
 open InputReader
 
-//[<Fact>]
-//let ``Solves part 1`` () =
-//    let input = readLines "./01/input"
-//    let result = part1 input
-//    Assert.Equal(0, part1)
+[<Fact>]
+let ``Finds numbers in a list that is larger than the previous number`` () =
+    let input = [199;200;208;210;200;207;240;269;260;263]
+    let result = findIncreasingNumbers input
+    let expected = [200;208;210;207;240;269;263]
+    Assert.Equal<int list>(expected, result)
 
+[<Fact>]
+let ``Solves part 1`` () =
+    let input = readNumberLines "./01/input"
+    let result = part1 input
+    Assert.Equal(1715, result)
--- aoc-2021/01/Day1.fs
+++ aoc-2021/01/Day1.fs
@@ -1,1 +1,13 @@
 module Day1
+
+let findIncreasingNumbers numbers =
+    numbers
+    |> Seq.pairwise
+    |> Seq.filter (fun (a, b) -> b > a)
+    |> Seq.map snd
+    |> Seq.toList
+
+let part1 input =
+    input
+    |> findIncreasingNumbers
+    |> Seq.length
