a5efcb53c1bd954db990bd355ada663406c97362
Carl-Johan Lindblad <cj.lindblad@caretotranslate.com>
2021-12-02 19:04:46 +0100

    Day 2 part 1 done.

--- aoc-2021/02/Day2.Tests.fs
+++ aoc-2021/02/Day2.Tests.fs
@@ -3,3 +3,40 @@ module Day2_Tests
 open Xunit
 open Day2
 open InputReader
+
+[<Fact>]
+let ``Applies forward instruction`` () =
+    let result = applyInstruction (0, 0) ("forward", 5)
+    Assert.Equal((5, 0), result)
+
+[<Fact>]
+let ``Applies up instruction`` () =
+    let result = applyInstruction (0, 0) ("up", 5)
+    Assert.Equal((0, -5), result)
+
+[<Fact>]
+let ``Applies down instruction`` () =
+    let result = applyInstruction (0, 0) ("down", 5)
+    Assert.Equal((0, 5), result)
+
+[<Fact>]
+let ``Applies list of instructions`` () =
+    let instructions = [
+        ("down", 5)
+        ("up", 1)
+        ("forward", 3)
+    ]
+    let result = applyInstructions instructions
+    Assert.Equal((3, 4), result)
+
+[<Fact>]
+let ``Solves part 1 with test input`` () =
+    let input = readLines "./02/test-input"
+    let result = part1 input
+    Assert.Equal(150, result)
+
+[<Fact>]
+let ``Solves part 1`` () =
+    let input = readLines "./02/input"
+    let result = part1 input
+    Assert.Equal(1694130, result)
--- aoc-2021/02/Day2.fs
+++ aoc-2021/02/Day2.fs
@@ -1,2 +1,21 @@
 module Day2
 
+let parseLine (line: string) =
+    line.Split()
+    |> (fun line -> (line[0], int line[1]))
+
+let applyInstruction ((horizontal, depth): (int * int)) ((direction, distance): (string * int)) =
+    match direction with
+    | "forward" -> (horizontal + distance, depth)
+    | "up" -> (horizontal, depth - distance)
+    | "down" -> (horizontal, depth + distance)
+    | _ -> (horizontal, depth)
+
+let applyInstructions (instructions: (string * int) seq) =
+    Seq.fold applyInstruction (0, 0) instructions
+
+let part1 input =
+    input
+    |> Seq.map parseLine
+    |> applyInstructions
+    |> (fun (a, b) -> a * b)
