6870942393344dd8c2631df2bfad9d3922363766
Carl-Johan Lindblad <cj.lindblad@caretotranslate.com>
2021-12-02 20:01:07 +0100

    Prepared day 3.

---
+++ aoc-2021/03/Day3.Tests.fs
@@ -0,0 +1,17 @@
+module Day3_Tests
+
+open Xunit
+open InputReader
+open Day3
+
+//[<Fact>]
+//let ``Solves part 1 with test input`` () =
+//    let input = readLines "./03/test-input"
+//    let result = part1 input
+//    Assert.Equal(0, result)
+//
+//[<Fact>]
+//let ``Solves part 1`` () =
+//    let input = readLines "./03/input"
+//    let result = part1 input
+//    Assert.Equal(0, result)
