c9658c2e1880fd66b6f71c4a68162e3ef07e1121
Carl-Johan Lindblad <cj.lindblad@caretotranslate.com>
2021-12-02 19:27:23 +0100

    Day 2 part 2 done.

--- aoc-2021/02/Day2.Tests.fs
+++ aoc-2021/02/Day2.Tests.fs
@@ -5,17 +5,17 @@ open Day2
 open InputReader
 
 [<Fact>]
-let ``Applies forward instruction`` () =
+let ``Applies forward instruction part 1`` () =
     let result = applyInstructionPart1 (0, 0) ("forward", 5)
     Assert.Equal((5, 0), result)
 
 [<Fact>]
-let ``Applies up instruction`` () =
+let ``Applies up instruction part 1`` () =
     let result = applyInstructionPart1 (0, 0) ("up", 5)
     Assert.Equal((0, -5), result)
 
 [<Fact>]
-let ``Applies down instruction`` () =
+let ``Applies down instruction part 1`` () =
     let result = applyInstructionPart1 (0, 0) ("down", 5)
     Assert.Equal((0, 5), result)
 
@@ -30,3 +30,15 @@ let ``Solves part 1`` () =
     let input = readLines "./02/input"
     let result = part1 input
     Assert.Equal(1694130, result)
+
+[<Fact>]
+let ``Solves part 2 with test input`` () =
+    let input = readLines "./02/test-input"
+    let result = part2 input
+    Assert.Equal(900, result)
+
+[<Fact>]
+let ``Solved part 2`` () =
+    let input = readLines "./02/input"
+    let result = part2 input
+    Assert.Equal(1698850445, result) 
--- aoc-2021/02/Day2.fs
+++ aoc-2021/02/Day2.fs
@@ -11,8 +11,21 @@ let applyInstructionPart1 ((horizontal, depth): int * int) ((direction, distance
     | "down" -> (horizontal, depth + distance)
     | _ -> (horizontal, depth)
 
+let applyInstructionPart2 ((horizontal, depth, aim): int * int * int) ((direction, distance): string * int) =
+    match direction with
+    | "forward" -> (horizontal + distance, depth + (aim * distance), aim)
+    | "up" -> (horizontal, depth, aim - distance)
+    | "down" -> (horizontal, depth, aim + distance)
+    | _ -> (horizontal, depth, aim)
+
 let part1 input =
     input
     |> Seq.map parseLine
     |> Seq.fold applyInstructionPart1 (0, 0)
     |> (fun (a, b) -> a * b)
+
+let part2 input =
+    input
+    |> Seq.map parseLine
+    |> Seq.fold applyInstructionPart2 (0, 0, 0)
+    |> (fun (a, b, _) -> a * b)
