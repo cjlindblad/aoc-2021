cff697c114fca8d8c0a3b21f999b1dfc2db97ed0
Carl-Johan Lindblad <cj.lindblad@caretotranslate.com>
2021-12-03 16:54:48 +0100

    Day 3 part 1 done.

--- aoc-2021/03/Day3.Tests.fs
+++ aoc-2021/03/Day3.Tests.fs
@@ -4,14 +4,15 @@ open Xunit
 open InputReader
 open Day3
 
-//[<Fact>]
-//let ``Solves part 1 with test input`` () =
-//    let input = readLines "./03/test-input"
-//    let result = part1 input
-//    Assert.Equal(0, result)
-//
-//[<Fact>]
-//let ``Solves part 1`` () =
-//    let input = readLines "./03/input"
-//    let result = part1 input
-//    Assert.Equal(0, result)
+
+[<Fact>]
+let ``Solves part 1 with test input`` () =
+    let input = readLines "./03/test-input"
+    let result = part1 input
+    Assert.Equal(198, result)
+
+[<Fact>]
+let ``Solves part 1`` () =
+    let input = readLines "./03/input"
+    let result = part1 input
+    Assert.Equal(3148794, result)
--- aoc-2021/03/Day3.fs
+++ aoc-2021/03/Day3.fs
@@ -1,2 +1,23 @@
 module Day3
 
+open System
+
+let binaryString (input: string seq) =
+    input
+    |> Seq.transpose
+    |> Seq.map (Seq.countBy id)
+    |> Seq.map (Seq.sortByDescending snd)
+    |> Seq.map Seq.head
+    |> Seq.map fst
+    |> Seq.map string
+    |> String.concat ""
+
+let flip (input: string) =
+    input
+    |> Seq.map (fun c -> if c = '0' then "1" else "0")
+    |> String.concat ""
+
+let part1 (input: string seq) =
+    let binary = binaryString input
+    let flippedBinary = flip binary
+    Convert.ToInt32(binary, 2) * Convert.ToInt32(flippedBinary, 2)
