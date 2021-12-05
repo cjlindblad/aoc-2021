0055f9bfc841a00e8d25c620d4b443d9e212c8a8
Carl-Johan Lindblad <cj.lindblad@caretotranslate.com>
2021-12-05 20:32:37 +0100

    Prepared day 6

---
+++ aoc-2021/06/Day6.Tests.fs
@@ -0,0 +1,11 @@
+module Day6_Tests
+
+open Xunit
+open InputReader
+open Day6
+
+//[<Fact>]
+//let ``Solves part 1 with test input`` () =
+//    let input = readLines "./06/test-input"
+//    let result = part1 input
+//    Assert.Equal(0, result)
