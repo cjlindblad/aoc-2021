1198924a2fe7d9e46a19c5c57f01f26d8ea723bc
Carl-Johan Lindblad <cj.lindblad@caretotranslate.com>
2021-12-05 20:27:19 +0100

    Refactored day 5.

--- aoc-2021/05/Day5.fs
+++ aoc-2021/05/Day5.fs
@@ -6,28 +6,22 @@ let parseLine input =
     let matches =
         Regex.Match(input, @"(\d+),(\d+) -> (\d+),(\d+)")
 
-    let x1 = matches.Groups[ 1 ].Value |> int
-    let y1 = matches.Groups[ 2 ].Value |> int
-    let x2 = matches.Groups[ 3 ].Value |> int
-    let y2 = matches.Groups[ 4 ].Value |> int
+    let x1 = matches.Groups[1].Value |> int
+    let y1 = matches.Groups[2].Value |> int
+    let x2 = matches.Groups[3].Value |> int
+    let y2 = matches.Groups[4].Value |> int
 
     ((x1, y1), (x2, y2))
 
-let makeLinesPart1 ((x1, y1), (x2, y2)) =
-    if x1 = x2 then
-        seq { for i in Seq.min [ y1; y2 ] .. Seq.max [ y1; y2 ] -> (x1, i) }
-    elif y1 = y2 then
-        seq { for i in Seq.min [ x1; x2 ] .. Seq.max [ x1; x2 ] -> (i, y1) }
-    else
-        Seq.empty
+let step n m = if n < m then 1 else - 1
 
-let makeLinesPart2 ((x1, y1), (x2, y2)) =
-    if x1 = x2 then
-        seq { for i in Seq.min [ y1; y2 ] .. Seq.max [ y1; y2 ] -> (x1, i) }
-    elif y1 = y2 then
-        seq { for i in Seq.min [ x1; x2 ] .. Seq.max [ x1; x2 ] -> (i, y1) }
-    else
-        Seq.zip [x1 .. (if x1 < x2 then 1 else -1) .. x2] [y1 .. (if y1 < y2 then 1 else -1) .. y2]
+let makeLines withDiagonals ((x1, y1), (x2, y2)) =
+    match (x1, y1) with
+    | x, _ when x = x2 -> seq { for i in Seq.min [ y1; y2 ] .. Seq.max [ y1; y2 ] -> (x, i) }
+    | _, y when y = y2 -> seq { for i in Seq.min [ x1; x2 ] .. Seq.max [ x1; x2 ] -> (i, y) }
+    | _ when withDiagonals = true ->
+        Seq.zip [ x1 .. step x1 x2 .. x2 ] [ y1 .. step y1 y2 .. y2 ]
+    | _ -> Seq.empty
 
 let overlappingPoints lines =
     lines
@@ -43,6 +37,6 @@ let solver lineMaker input =
     |> overlappingPoints
     |> Seq.length
 
-let part1: (seq<string> -> int) = solver makeLinesPart1
+let part1: (seq<string> -> int) = solver (makeLines false)
 
-let part2: (seq<string> -> int) = solver makeLinesPart2
+let part2: (seq<string> -> int) = solver (makeLines true)
