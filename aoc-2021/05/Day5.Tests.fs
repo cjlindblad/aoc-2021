f6350375aad864728a7e229d0f9c633b6f8f4d42
Carl-Johan Lindblad <cj.lindblad@caretotranslate.com>
2021-12-05 11:19:33 +0100

    Day 5 part 2 done.

--- aoc-2021/05/Day5.Tests.fs
+++ aoc-2021/05/Day5.Tests.fs
@@ -16,3 +16,14 @@ let ``Solves part 1`` () =
     let result = part1 input
     Assert.Equal(5442, result)
 
+[<Fact>]
+let ``Solves part 2 with test input`` () =
+    let input = readLines "./05/test-input"
+    let result = part2 input
+    Assert.Equal(12, result)
+
+[<Fact>]
+let ``Solves part 2`` () =
+    let input = readLines "./05/input"
+    let result = part2 input
+    Assert.Equal(19571, result)
--- aoc-2021/05/Day5.fs
+++ aoc-2021/05/Day5.fs
@@ -2,10 +2,6 @@ module Day5
 
 open System.Text.RegularExpressions
 
-let wat =
-    [ [ (1, 2); (1, 3); (1, 4); (1, 5) ]
-      [ (1, 3); (1, 4) ] ]
-
 let parseLine input =
     let matches =
         Regex.Match(input, @"(\d+),(\d+) -> (\d+),(\d+)")
@@ -22,6 +18,22 @@ let parseLine input =
     else
         Seq.empty
 
+let parseLinePart2 input =
+    let matches =
+        Regex.Match(input, @"(\d+),(\d+) -> (\d+),(\d+)")
+
+    let x1 = matches.Groups[ 1 ].Value |> int
+    let y1 = matches.Groups[ 2 ].Value |> int
+    let x2 = matches.Groups[ 3 ].Value |> int
+    let y2 = matches.Groups[ 4 ].Value |> int
+
+    if x1 = x2 then
+        seq { for i in Seq.min [ y1; y2 ] .. Seq.max [ y1; y2 ] -> (x1, i) }
+    elif y1 = y2 then
+        seq { for i in Seq.min [ x1; x2 ] .. Seq.max [ x1; x2 ] -> (i, y1) }
+    else
+        Seq.zip [x1 .. (if x1 < x2 then 1 else -1) .. x2] [y1 .. (if y1 < y2 then 1 else -1) .. y2]
+
 let overlappingPoints lines =
     lines
     |> Seq.concat
@@ -32,5 +44,11 @@ let overlappingPoints lines =
 let part1 input =
     input
     |> Seq.map parseLine
+    |> overlappingPoints
+    |> Seq.length
+
+let part2 input =
+    input
+    |> Seq.map parseLinePart2
     |> overlappingPoints
     |> Seq.length
