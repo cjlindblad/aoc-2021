0c4779b82383bfe7878b39b75d7ef1b1f1e052e0
Carl-Johan Lindblad <cj.lindblad@caretotranslate.com>
2021-12-06 08:01:15 +0100

    Day 6 part 1 done.

--- aoc-2021/06/Day6.Tests.fs
+++ aoc-2021/06/Day6.Tests.fs
@@ -4,8 +4,24 @@ open Xunit
 open InputReader
 open Day6
 
-//[<Fact>]
-//let ``Solves part 1 with test input`` () =
-//    let input = readLines "./06/test-input"
-//    let result = part1 input
-//    Assert.Equal(0, result)
+[<Theory>]
+[<InlineData("3,4,3,1,2", "2,3,2,0,1")>]
+[<InlineData("2,3,2,0,1", "1,2,1,6,0,8")>]
+[<InlineData("0,1,0,5,6,7,8", "6,0,6,4,5,6,7,8,8")>]
+[<InlineData("0,1,0,5,6,0,1,2,2,3,0,1,2,2,2,3,3,4,4,5,7,8", "6,0,6,4,5,6,0,1,1,2,6,0,1,1,1,2,2,3,3,4,6,7,8,8,8,8")>]
+let ``Gets next day for input`` (input, expected) =
+    let result = nextDay (parseInput input)
+    let expected = parseInput expected
+    Assert.Equal<seq<int>>(expected, result)
+
+[<Fact>]
+let ``Solves part 1 with test input`` () =
+    let input = readLines "./06/test-input" |> Seq.head
+    let result = part1 input
+    Assert.Equal(5934, result)
+
+[<Fact>]
+let ``Solves part 1 with input`` () =
+    let input = readLines "./06/input" |> Seq.head
+    let result = part1 input
+    Assert.Equal(352195, result)
--- aoc-2021/06/Day6.fs
+++ aoc-2021/06/Day6.fs
@@ -1,2 +1,22 @@
 module Day6
 
+let parseInput (input: string) =
+    input.Split(",") |> Array.map int
+
+let nextDay fish =
+    let newFish =
+        fish
+        |> Array.filter (fun x -> x = 0)
+        |> Array.map (fun _ -> 8)
+    let nextFish =
+        fish
+        |> Array.map (fun x -> if x = 0 then 6 else x - 1)
+    
+    Array.append nextFish newFish
+
+let part1 input =
+    let initialState = parseInput input
+    [|1 .. 80|]
+    |> Array.fold (fun acc _ ->
+        nextDay acc) initialState
+    |> Array.length
